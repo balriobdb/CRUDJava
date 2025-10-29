3368d24257d84d085ff530c5a8ea3082f5530e0e
balriobdb <mauriciofreitasb3@gmail.com>
2025-10-28 21:41:27 -0300

    Implementado delete produto
    Implementação do método delete no ProdutoDAO

--- src/main/java/DAO/ProdutoDAO.java
+++ src/main/java/DAO/ProdutoDAO.java
@@ -89,4 +89,22 @@ public class ProdutoDAO {
             Conexao.closeConnection(con, stmt);
         }
     }
+
+    public void delete(Produto p) {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("DELETE FROM tbl_produto WHERE id = ?");
+
+            stmt.setInt(1, p.getId());
+
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Produto removido com sucesso!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao remover: " + e);
+        } finally {
+            Conexao.closeConnection(con, stmt);
+        }
+    }
 }
